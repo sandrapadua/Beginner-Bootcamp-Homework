e3008d641460de9fa5d1a5a9d1056592c87e0c4f
Sandra Padua <sandrapadua5@gmail.com>
2019-02-16 00:53:52 +0100

    completed till ui for rest

--- BB-HW/YOUR_JAVASCRIPT_HERE.js
+++ BB-HW/YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,20 @@
-// Write your JS here
+const hero = {
+    name : 'Super Mario',
+    heroic : true,
+    inventory : [],
+    health : 10,
+    weapon : {type : 'gun',
+             damage :2
+             }
+}
+function rest(hero){
+hero.health = 10
+return hero
+}
+function pickUpItem(){
+
+}
+function equipWeapon(){
+
+}
+
