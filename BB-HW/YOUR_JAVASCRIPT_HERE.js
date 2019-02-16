cdb2938d7a4db117dd97514ea84bb797dcd09c17
Sandra Padua <sandrapadua5@gmail.com>
2019-02-16 15:23:36 +0100

    completed test of Run the tests button

--- BB-HW/YOUR_JAVASCRIPT_HERE.js
+++ BB-HW/YOUR_JAVASCRIPT_HERE.js
@@ -7,14 +7,22 @@ const hero = {
              damage :2
              }
 }
+const weaponObject = {type:'dagger',
+    damage:2}
 function rest(hero){
 hero.health = 10
 return hero
 }
-function pickUpItem(){
+
+function pickUpItem(objHero,object){
+objHero.inventory.push(object)
 
 }
-function equipWeapon(){
 
+function equipWeapon(objHero){
+
+var InventorySize = objHero.inventory.length
+if(InventorySize > 0){
+    objHero.weapon = objHero.inventory[0]
 }
-
+}
